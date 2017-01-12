a65eb9734927ce2f0943e16d4b958024779335e9
Chae Jin Jun <cjun@incontrol.onmicrosoft.com>
2017-01-13 09:50:10 +1300

    initialising

---
+++ src/ICT_BLE.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+    scan: function (services, seconds, success, failure) {
+        var successWrapper = function (peripheral) {
+            convertToNativeJS(peripheral);
+            success(peripheral);
+        };
+        cordova.exec(successWrapper, failure, 'BLE', 'scan', [services, seconds]);
+    },
+}
