e394361cbffe8520630c23d4c4dd28c2527fdc26
Chae Jin Jun <cjun@incontrol.onmicrosoft.com>
2017-01-13 12:33:25 +1300

    init

--- src/android/MainPlugin.java
+++ src/android/MainPlugin.java
@@ -1,11 +1,15 @@
 package com.cojj.cordova.ble.service;
 
 import org.apache.cordova.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.util.Log;
+import android.os.Handler;
 
 public class MainPlugin extends CordovaPlugin {
     private static final String TAG = "ICT BLE";
+    private CallbackContext callbackContext;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -16,6 +20,28 @@ public class MainPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
         Log.e(TAG, "BLE: " + action);
+        this.callbackContext = callbackContext;
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                MainPlugin.this.callbackContext.success();
+            }
+        }, 5000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                MainPlugin.this.callbackContext.success();
+            }
+        }, 10000);
+
+        /*
+        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        result.setKeepCallback(true);
+        callbackContext.sendPluginResult(result);
+        */
         return true;
     }
 
