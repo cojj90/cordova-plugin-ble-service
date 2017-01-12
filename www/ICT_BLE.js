0b9bc4628a571c83b7ec24aa498847cfb64e6a7e
Chae Jin Jun <cjun@incontrol.onmicrosoft.com>
2017-01-13 10:56:36 +1300

    init

--- www/ICT_BLE.js
+++ www/ICT_BLE.js
@@ -1,15 +1,7 @@
 "use strict";
 
 module.exports = {
-    scan: function (services, seconds, success, failure) {
-        var successWrapper = function (peripheral) {
-            convertToNativeJS(peripheral);
-            success(peripheral);
-        };
-        cordova.exec(successWrapper, failure, 'BLE', 'scan', [services, seconds]);
+    scan: function () {
+        console.alert("SCAN");
     }
 };
-
-module.exports.withPromises = {
-    scan: module.exports.scan
-}
